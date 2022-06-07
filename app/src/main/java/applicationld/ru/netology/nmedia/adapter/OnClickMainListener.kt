df3ea03436fec030cf1be7404cc4c8cb035feb21
SerKit163 <dsm-git@ya.ru>
2022-06-07 23:23:12 +0400

    lesson #6 RecyclerView

---
+++ app/src/main/java/applicationld/ru/netology/nmedia/adapter/OnClickMainListener.kt
@@ -0,0 +1,6 @@
+package applicationld.ru.netology.nmedia.adapter
+
+interface OnClickMainListener {
+    fun onLiked(id: Long)
+    fun onShared(id: Long)
+}
---
+++ app/src/main/java/applicationld/ru/netology/nmedia/adapter/PostsAdapter.kt
@@ -0,0 +1,22 @@
+package applicationld.ru.netology.nmedia.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import applicationld.ru.netology.nmedia.Post
+import applicationld.ru.netology.nmedia.databinding.PostCardBinding
+
+class PostsAdapter(
+    private val onClickMainListener: OnClickMainListener
+) : ListAdapter<Post, PostViewHolder>(PostDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val binding = PostCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PostViewHolder(binding, onClickMainListener)
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        val post = getItem(position)
+        holder.bind(post)
+    }
+}
